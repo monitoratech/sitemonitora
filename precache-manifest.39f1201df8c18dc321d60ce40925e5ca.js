a0ba9d287322cff1d24203c9b8156af5d993235d
antonioducs <antonioduarte.cs@gmail.com>
2020-01-19 13:15:52 -0300

    Updates

---
+++ precache-manifest.39f1201df8c18dc321d60ce40925e5ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc2903885c8f057e53ec53b5f134c6e",
+    "url": "/sitemonitora/index.html"
+  },
+  {
+    "revision": "4f57860a8c69e8c68d8e",
+    "url": "/sitemonitora/static/css/main.b3857b34.chunk.css"
+  },
+  {
+    "revision": "3f095e51e1a4cd1c9820",
+    "url": "/sitemonitora/static/js/2.807c55d6.chunk.js"
+  },
+  {
+    "revision": "4f57860a8c69e8c68d8e",
+    "url": "/sitemonitora/static/js/main.c241fa42.chunk.js"
+  },
+  {
+    "revision": "c57779e4209fd7f30b89",
+    "url": "/sitemonitora/static/js/runtime-main.2cc92a1c.js"
+  },
+  {
+    "revision": "af0ba8ed3e06a90d126aedfa9e400066",
+    "url": "/sitemonitora/static/media/LogoNameFraseBlack.af0ba8ed.png"
+  },
+  {
+    "revision": "914aa985b145514d4b1a2864984519b2",
+    "url": "/sitemonitora/static/media/LogoNameFraseBlue@0,5x.914aa985.png"
+  },
+  {
+    "revision": "3b7ab5e4e9fd6df9fc08da0f670afc4a",
+    "url": "/sitemonitora/static/media/bakcgroundContactMonitora.3b7ab5e4.png"
+  },
+  {
+    "revision": "4a941d640a347d3876e3c606d6ec0e2b",
+    "url": "/sitemonitora/static/media/leelawadee-ui.4a941d64.ttf"
+  }
+]);
