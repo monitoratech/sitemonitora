d638992e7d5e6af7b3bb5d0b7cd272151aaae698
antonioducs <antonioduarte.cs@gmail.com>
2020-01-21 22:20:34 -0300

    att url card

--- src/pages/Card/index.js
+++ src/pages/Card/index.js
@@ -4,7 +4,7 @@ export default class index extends Component {
 
     render() {
         return (
-            <meta http-equiv="refresh" content="1;URL=https://github.com/monitoratech/websiteback/raw/master/Monitora.pdf" />
+            <meta http-equiv="refresh" content="1;URL=https://github.com/monitoratech/sitemonitora/raw/master/src/assets/Monitora.pdf" />
         );
     }
 }
