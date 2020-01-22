c7daa943aa990418cc7f7f4c941c612ddde59980
antonioducs <antonioduarte.cs@gmail.com>
2020-01-21 22:31:18 -0300

    correção url

--- src/utils/funcSendMail.js
+++ src/utils/funcSendMail.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async function sendMail(name, email, tel, theme, msg) {
 
-    await axios.post("https://monitora-app.herokuapp.com/api/mailto",{
+    await axios.post("http://monitora-app.herokuapp.com/api/mailto",{
         name: name,
         email: email,
         tel: tel,
